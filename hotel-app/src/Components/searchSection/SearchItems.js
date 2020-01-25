93099583c334c2f1dac691dbc342b80db8400ffe
unknown <medhajaiswal@gmail.com>
2020-01-25 09:17:52 +0000

    commiting form changes

--- hotel-app/src/Components/searchSection/SearchItems.js
+++ hotel-app/src/Components/searchSection/SearchItems.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
 import * as constant from '../../helper/constant'
 
 export const SearchItems = () => {
@@ -9,7 +9,7 @@ export const SearchItems = () => {
                 <Form.Control type="text" placeholder={constant.SEARCH_PLACEHOLDER} />
             </Form.Group>
             <Form.Group controlId="star-filter">
-                <Form.Label>Stars</Form.Label>
+                <Form.Label>{constant.STARS}</Form.Label>
                 <Form.Control as="select">
                 <option>All</option>
                 <option>1</option>
@@ -20,11 +20,11 @@ export const SearchItems = () => {
                 </Form.Control>
             </Form.Group>
             <Form.Group controlId="price-filter">
-                <Form.Label>Price range</Form.Label>
+                <Form.Label>{constant.PRICE_RANGE}</Form.Label>
                 <Form.Control type="range" min="500" max="10000"/>
             </Form.Group>
             <Button variant="primary" type="submit">
-                Search
+                {constant.SEARCH}
             </Button>
         </Form>
     )
